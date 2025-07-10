5655b9a82c7db235f56bc44af9b1264d7e3b77ea
MathKing1 <matheus_n_cardoso@estudante.sesisenai.org.br>
2025-07-10 09:54:58 -0300

    Add files via upload

---
+++ lista2/index2.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <title>Tabuada</title>
+</head>
+<body>
+
+    <h2>Calcular a Tabuada</h2>
+
+    <form method="post" action="">
+        <label for="numero">Digite um número:</label>
+        <input type="number" name="numero" id="numero" required>
+        <input type="submit" value="Calcular">
+    </form>
+
+    <?php
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        $numero = $_POST["numero"];
+
+        echo "<h3>Tabuada do $numero:</h3>";
+
+        for ($i = 1; $i <= 10; $i++) {
+            $resultado = $numero * $i;
+            echo "$numero x $i = $resultado<br>";
+        }
+    }
+    ?>
+
+</body>
+</html>
---
+++ lista3/index3.php
@@ -0,0 +1,10 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Informar se um número é positivo, negativo ou zero.</title>
+</head>
+<body>
+    
+</body>
+</html>
